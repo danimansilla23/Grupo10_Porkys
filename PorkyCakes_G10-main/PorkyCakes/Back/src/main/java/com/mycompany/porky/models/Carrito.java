9956678f75ef958f102038fd760601ec1bdd31bd
IgnacioCasteran <nacho_casteran@hotmail.com.ar>
2024-11-20 08:56:28 -0300

    Add files via upload

--- PorkyCakes_G10-main/PorkyCakes/Back/src/main/java/com/mycompany/porky/models/Carrito.java
+++ PorkyCakes_G10-main/PorkyCakes/Back/src/main/java/com/mycompany/porky/models/Carrito.java
@@ -8,14 +8,12 @@ import lombok.Data;
 @Data
 public class Carrito {
     private Integer idCarrito;
-    private Integer Estado;
     private Date Fecha_creacion;
     private Integer Clientes_idCliente;
     private Integer Reservas_idReservas;
 
    public Carrito(Integer idCarrito, Integer IdCliente){
     this.idCarrito = idCarrito;
-    this.Estado = 1; 
     this.Fecha_creacion = new Date(System.currentTimeMillis()); // Fecha actual
     this.Clientes_idCliente = IdCliente;
    }
